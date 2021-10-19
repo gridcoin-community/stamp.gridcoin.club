7526927ada91d2f9fd264a962f8539470ba66fef
Grid Cat <gridcat@protonmail.com>
2021-10-19 10:21:54 +0200

    feat(footer): tune footer

--- packages/grc-stamp-frontend/src/components/Footer/Footer.tsx
+++ packages/grc-stamp-frontend/src/components/Footer/Footer.tsx
@@ -2,23 +2,28 @@ import {
   Container,
   Divider,
   Typography,
-  Box,
+  Grid,
 } from '@mui/material';
-import { Balance } from 'components/Balance';
+import { Balance } from 'components/Footer/Balance';
 import React from 'react';
 
 export function Footer() {
   return (
     <Container maxWidth="xl">
       <Divider />
-      <Box sx={{ display: 'flex', mt: 4, mb: 4 }}>
-        <Typography variant="caption">
-          Made with ❤ by @gridcat
-        </Typography>
-        <Typography variant="caption" sx={{ flexGrow: 1, justifyContent: 'flex-end', display: 'flex' }}>
-          <Balance />
-        </Typography>
-      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="caption">
+            Made with ❤ by @gridcat
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Typography variant="caption" sx={{ textAlign: 'right' }}>
+            <Balance />
+          </Typography>
+        </Grid>
+      </Grid>
+      <br />
     </Container>
   );
 }
