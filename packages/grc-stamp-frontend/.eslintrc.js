1fd53b2766237d6d7337b30a599a23572c555f88
Grid Cat <gridcat@protonmail.com>
2021-10-19 09:34:37 +0200

    style(eslint): tune eslint rules

--- packages/grc-stamp-frontend/.eslintrc.js
+++ packages/grc-stamp-frontend/.eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     'react/jsx-props-no-spreading': 0,
     'comma-dangle': ["error", "always-multiline"],
     'import/prefer-default-export': 0,
-    'no-case-declarations': 0
+    'no-case-declarations': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'no-plusplus': 0
   },
 };
