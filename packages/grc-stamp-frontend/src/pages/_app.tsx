eaeabbf1fc5e99b0ee94456bcb58af12bf360667
Grid Cat <gridcat@protonmail.com>
2021-10-21 16:03:26 +0200

    feat: add tracker

--- packages/grc-stamp-frontend/src/pages/_app.tsx
+++ packages/grc-stamp-frontend/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
+import Script from 'next/script';
 import theme from '../theme';
 import createEmotionCache from '../createEmotionCache';
 import '../styles/style.css';
@@ -23,6 +24,9 @@ export default function MyApp(props: MyAppProps) {
         <title>My page</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+      {process.env.NEXT_PUBLIC_TRACK === 'true' && (
+      <Script src="https://daj.pw/js/plausible.js" data-domain="stamp.gridcoin.club" />
+      )}
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
