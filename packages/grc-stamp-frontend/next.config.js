257e4201b49af640dbb1b6e760f356059bcb17f8
Grid Cat <gridcat@protonmail.com>
2021-10-21 09:39:35 +0200

    fix(config): tweak config

--- packages/grc-stamp-frontend/next.config.js
+++ packages/grc-stamp-frontend/next.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    API_URL: 'http://localhost:7000',
-    EXPLORER_BLOCK_URL: 'https://testnet.gridcoinstats.eu/block/[data]',
-    EXPLORER_TX_URL: 'https://testnet.gridcoinstats.eu/tx/[data]',
+    API_URL: process.env.API_URL || 'http://localhost:7000',
+    EXPLORER_BLOCK_URL: process.env.EXPLORER_BLOCK_URL || 'https://testnet.gridcoinstats.eu/block/[data]',
+    EXPLORER_TX_URL: process.env.EXPLORER_TX_URL || 'https://testnet.gridcoinstats.eu/tx/[data]',
   }
 };
