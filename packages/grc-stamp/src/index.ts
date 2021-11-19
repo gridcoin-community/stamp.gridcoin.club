ff71e4f5d33e52203a0a76d61e2256798784874d
Grid Cat <gridcat@protonmail.com>
2021-11-19 21:45:24 +0100

    refactor: remove local test rubbish

--- packages/grc-stamp/src/index.ts
+++ packages/grc-stamp/src/index.ts
@@ -1,4 +1,3 @@
-import crypto from 'crypto';
 import { config } from './config';
 import { connect } from './lib/gridcoin';
 import { log } from './lib/log';
@@ -20,9 +19,6 @@ async function main(): Promise<void> {
   const scraper = new Scraper();
   // run scraper once per minute
   setInterval(() => scraper.scrape(), config.SCRAPER_TIMEOUT);
-  const hash = `${Math.random() * 100000000000000}`;
-  const sha = crypto.createHash('sha256');
-  sha.update(hash);
   setInterval(() => stampService.publishStamp(), config.PUBLISH_TIMEOUT);
 }
 
