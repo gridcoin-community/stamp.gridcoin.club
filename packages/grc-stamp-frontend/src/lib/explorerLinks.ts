86482c1e9023860863bc01f33831631d7d61f32e
Grid Cat <gridcat@protonmail.com>
2026-05-05 19:41:06 +0200

    fix: get rid of nasty javascript voids for empty links

--- packages/grc-stamp-frontend/src/lib/explorerLinks.ts
+++ packages/grc-stamp-frontend/src/lib/explorerLinks.ts
@@ -1,15 +1,13 @@
-export function txUrl(tx: string | undefined) {
+export function txUrl(tx: string | undefined): string | undefined {
   if (tx) {
     return process.env.NEXT_PUBLIC_EXPLORER_TX_URL?.replace(/\[data\]/, String(tx));
   }
-   
-  return 'javascript:void(0);';
+  return undefined;
 }
 
-export function blockUrl(block: number | undefined) {
+export function blockUrl(block: number | undefined): string | undefined {
   if (block) {
     return process.env.NEXT_PUBLIC_EXPLORER_BLOCK_URL?.replace(/\[data\]/, String(block));
   }
-   
-  return 'javascript:void(0);';
+  return undefined;
 }
